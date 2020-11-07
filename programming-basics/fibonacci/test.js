721fe38d835e9f28b2da377c4da24259c33a85cc
pedro.silva <pedro.silva@catho.com>
2020-11-07 17:30:37 -0300

    enh: fibonacci exercise

--- programming-basics/fibonacci/test.js
+++ programming-basics/fibonacci/test.js
@@ -1,18 +1,28 @@
-import fibb from './main'
+import fib from './main'
 
 describe('programming-basics[001]', () => {
-  it(`should calculate fibbonacci of 0`, () => {
-    const result = fibb(0)
+  it(`should calculate fibonacci of 0`, () => {
+    const result = fib(1)
     expect(result).toBe(1)
   })
 
-  it(`should calculate fibbonacci of 1`, () => {
-    const result = fibb(1)
+  it(`should calculate fibonacci of 1`, () => {
+    const result = fib(2)
     expect(result).toBe(1)
   })
 
-  it(`should calculate fibbonacci of 10`, () => {
-    const result = fibb(10)
-    expect(result).toBe()
+  it(`should calculate fibonacci of 10`, () => {
+    const result = fib(10)
+    expect(result).toBe(55)
+  })
+
+  it(`should calculate fibonacci of 100`, () => {
+    const result = fib(20)
+    expect(result).toBe(6765)
+  })
+
+  it(`should calculate fibonacci of -1`, () => {
+    const result = fib(-1)
+    expect(result).toBe(0)
   })
 })
