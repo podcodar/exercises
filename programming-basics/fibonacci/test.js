68f55901f0b1fa5ab2f5ebb52ee639a1c3f809b8
pedro.silva <pedro.silva@catho.com>
2020-11-11 19:37:09 -0300

    refactor: traduz textos para portugues

--- programming-basics/fibonacci/test.js
+++ programming-basics/fibonacci/test.js
@@ -1,23 +1,23 @@
 import fib from "./main";
 
-describe("Tests - Fibonacci", () => {
-  it(`should calculate fibonacci of 0`, () => {
+describe("Testes - Fibonacci", () => {
+  it(`deve calcular o valor da sequencia de fibonacci para 0`, () => {
     expect(fib(1)).toBe(1);
   });
 
-  it(`should calculate fibonacci of 1`, () => {
+  it(`deve calcular o valor da sequencia de fibonacci para 1`, () => {
     expect(fib(2)).toBe(1);
   });
 
-  it(`should calculate fibonacci of 10`, () => {
+  it(`deve calcular o valor da sequencia de fibonacci para 10`, () => {
     expect(fib(10)).toBe(55);
   });
 
-  it(`should calculate fibonacci of 100`, () => {
+  it(`deve calcular o valor da sequencia de fibonacci para 20`, () => {
     expect(fib(20)).toBe(6765);
   });
 
-  it(`should calculate fibonacci of negative numbers`, () => {
+  it(`deve calcular o valor da sequencia de fibonacci para negative numbers`, () => {
     expect(fib(-1)).toBe(undefined);
     expect(fib(-100)).toBe(undefined);
     expect(fib(-Infinity)).toBe(undefined);
