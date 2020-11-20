7af4ab86bc6962395e0320cb015ed1c1be71bc09
Ana Luiza Caldas <analuizacaldas7@gmail.com>
2020-11-19 21:41:21 -0300

    exe: palindromo

--- programming-basics/palindromo/main.js
+++ programming-basics/palindromo/main.js
@@ -1,6 +1,14 @@
-const palindromo = (primeiraPalavra, segundaPalavra) => {
-  // insira seu código aqui e retorne a resposta
-  return; //resposta
-};
+function palindromo (palavra){
+  //troca os espaços por uma string vazia
+  palavra = palavra.replace(/ /g, '')
+  //só a metade da palavra é suficiente para saber se é um palindromo
+  for (let i = 0; i < palavra.length/2; i++){
+  //compara uma posição com a posição oposta - tamanho da palavra - i - 1 
+    if(palavra[palavra.length-i-1] != palavra[i]){
+      return false;
+    }
+  }
+  return true;   
+}
 
 export default palindromo;
