cd7b2e4a32a31c6e1d57db21d8dbc92d1240bb04
nato-re <alvares.renato@hotmail.com>
2020-12-06 23:32:21 -0300

    deal with CR sugestions and try to aply them, problem with 3 test

--- programming-basics/dorminhoco/main.js
+++ programming-basics/dorminhoco/main.js
@@ -1,24 +1,21 @@
-const dreams = [
+const dreamList = [
   'Ovelhas astronautas',
   'Tava jogando mesmo',
   'Sonhei com código',
-  'Mozão'
-]
-const randomPosition = Math.floor(Math.random() * dreams.length)
-const randomDream = (resolve) => () => resolve(dreams[randomPosition])
+  'Mozão',
+];
 
-function sleep(ms) {
-  return new Promise(
-    (resolve) => setTimeout(randomDream(resolve), ms)
+const randomDream = (dreams, callback) =>
+callback(dreams[Math.floor(Math.random() * dreams.length)]);
+
+const sleep = (ms, cb) => setTimeout(cb, ms);
+
+const dorminhoco = async (milliseconds, dreamList) => {
+  const promisedDream = new Promise((resolve) =>
+    sleep(milliseconds, randomDream(dreamList, resolve))
   );
-}
-
-const dorminhoco = async (valor) => {
-
-  const sonho = await sleep(2000)
-
-  return sonho
+  const dream = await promisedDream;
+  return dream;
 };
 
-
-module.exports = { dorminhoco, sleep, randomDream };
+module.exports = { dorminhoco };
--- programming-basics/dorminhoco/test.js
+++ programming-basics/dorminhoco/test.js
@@ -1,31 +1,26 @@
 const main = require('./main');
 
-
 describe('Testes - Dominhoco', () => {
-
   afterEach(() => {
     jest.clearAllMocks();
     jest.useFakeTimers();
+  });
+  const dreamMock = ['não lembro do sonho'];
 
-  })
   it(`resultado deve ser uma string`, async () => {
-    // const mockDream = jest.fn().mockReturnValue('Ovelhas astronautas')
-
-    const sonho = await main.dorminhoco();
-    expect(typeof sonho).toBe('string');
+    const dream = await main.dorminhoco(42, dreamMock);
+    expect(typeof dream).toBe('string');
+    expect(dream).toBe(dreamMock[0]);
   });
 
-  it(`resultado deve ser uma string`, async () => {
-    main.dorminhoco();
-    
-    expect(setTimeout).toBeCalledTimes(1)
-    
+  it(`setTimeout dever ser sido chamado uma vez`, async () => {
+    main.dorminhoco(42, dreamMock);
+
+    expect(setTimeout).toBeCalledTimes(1);
   });
   it(`espera que setTimeout seja chamado com uma função`, async () => {
-    main.dorminhoco();
+    main.dorminhoco(42, dreamMock);
 
-    expect(setTimeout).toBeCalledWith(expect.any(Function), 2000)
-    
-    
+    expect(setTimeout).toBeCalledWith(expect.any(Function), 42);
   });
 });
